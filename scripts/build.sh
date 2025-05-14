00e976df613969c622a6cc80d5bd165ed9c30307
akhaled01 <akhaledlarus@gmail.com>
2025-05-14 21:54:50 +0300

    build: update build script to use release profile and add helpful messages

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,15 +2,18 @@
 
 # if on macos, set the library path
 if [ "$(uname)" = "Darwin" ]; then
+    echo "Setting library path for macos, please ensure you have installed sdl2 via brew"
     export LIBRARY_PATH="/opt/homebrew/lib"
     export CFLAGS="-I/opt/homebrew/include"
     export LDFLAGS="-L/opt/homebrew/lib"
 fi
 
-cargo build --bin server
-cargo build --bin client
+cargo build --bin server --release
+cargo build --bin client --release
 
 # extract the binaries and place in bin folder
 mkdir -p bin
-cp target/debug/server bin/server
-cp target/debug/client bin/client
+cp target/release/server bin/server
+cp target/release/client bin/client
+
+echo "Build complete"
