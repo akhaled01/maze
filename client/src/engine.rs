133afbc03e7bfcba86bb7f0a6bf786742d72590e
jackblue2009 <arahmanbucheeri@gmail.com>
2025-05-21 13:45:30 +0300

    my work in progress here, will finish this within a week inshallah

---
+++ client/src/engine.rs
@@ -0,0 +1,13 @@
+pub struct Engine;
+
+impl Engine {
+    pub fn new() -> Self {
+        // Initialize SDL, OpenGL, load assets, etc.
+        Self
+    }
+
+    pub fn run(&mut self) {
+        // Main game loop
+        println!("Engine running!");
+    }
+}
